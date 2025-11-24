40f46d0a74f298e05e0bc081ac412e301c210448
enhanced <enhanced@enhanced.enhanced>
2025-11-24 17:09:49 +0800

    enhanced

--- src/base/bittorrent/sessionimpl.cpp
+++ src/base/bittorrent/sessionimpl.cpp
@@ -128,7 +128,7 @@ const std::chrono::seconds FREEDISKSPACE_CHECK_TIMEOUT = 30s;
 namespace
 {
     const char PEER_ID[] = "qB";
-    const auto USER_AGENT = QStringLiteral("qBittorrent Enhanced/" QBT_VERSION_2);
+    const auto USER_AGENT = QStringLiteral("qBittorrent/5.1.4");
     const QString DEFAULT_DHT_BOOTSTRAP_NODES = u"dht.libtorrent.org:25401, dht.transmissionbt.com:6881, router.bittorrent.com:6881"_s;
 
     void torrentQueuePositionUp(const lt::torrent_handle &handle)
@@ -1725,7 +1725,7 @@ void SessionImpl::initializeNativeSession()
 {
     lt::settings_pack pack = loadLTSettings();
 
-    const std::string peerId = lt::generate_fingerprint(PEER_ID, QBT_VERSION_MAJOR, QBT_VERSION_MINOR, QBT_VERSION_BUGFIX, QBT_VERSION_BUILD);
+    const std::string peerId = lt::generate_fingerprint(PEER_ID, 5, 1, 4, 0);
     pack.set_str(lt::settings_pack::peer_fingerprint, peerId);
 
     pack.set_bool(lt::settings_pack::listen_system_port_fallback, false);
